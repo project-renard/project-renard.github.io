e7f9bfaaa84e3351fc0e001502d2c30235b7d63c
Zakariyya Mughal <zaki.mughal@gmail.com>
2016-02-17 03:12:47 -0600

    Create plugin that adds GitHub edit page link

---
+++ .ikiwiki-plugin-github/IkiWiki/Plugin/github.pm
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+package IkiWiki::Plugin::github;
+
+use warnings;
+use strict;
+use IkiWiki 3.00;
+
+sub import {
+	hook(type => "pagetemplate", id => "skeleton", call => \&pagetemplate);
+}
+
+sub pagetemplate (@) {
+	my %params=@_;
+	my $page=$params{page};
+	my $template=$params{template};
+
+	my $page_source = $pagesources{ $page };
+	return unless $page_source;
+
+	my $github_config = $config{github};
+
+	my $editurl = "https://github.com/@{[ $github_config->{repo} ]}/edit/@{[ $github_config->{branch} ]}/$page_source";
+
+	$template->param( have_actions => 1 );
+	$template->param( editurl => $editurl );
+}
+
+1
